0d7a00eccc149b202e7848b863f2cafb2a4ff9d4
*Diogo <diogolimakas@hotmail.com>
2022-01-02 00:22:47 -0300

    Otimização e envio de formulario

--- js/script.js
+++ js/script.js
@@ -13,12 +13,9 @@ links.forEach(ativarLink);
 
 // Ativar Items do Orçamento
 
-
-
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-
   const elemento = document.getElementById(parametro);
   if (elemento) {
     elemento.checked = true;
@@ -27,54 +24,44 @@ function ativarProduto(parametro) {
 
 parametros.forEach(ativarProduto);
 
-//perguntas Frequêntes
-
-const perguntas = document.querySelectorAll('.perguntas button');
-
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
   const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute('aria-controls');
+  const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
-  resposta.classList.toggle('ativa')
-  const ativa = resposta.classList.contains('ativa')
-  pergunta.setAttribute('aria-expanded', ativa)
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener('click', ativarPergunta)
+  pergunta.addEventListener("click", ativarPergunta);
 }
-perguntas.forEach(eventosPerguntas)
 
+perguntas.forEach(eventosPerguntas);
 
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-
-
-//  Galeria de Bicicletas
-
-
-
-const galeria = document.querySelectorAll('.bicicleta-imagens img')
-const galeriaContainer = document.querySelector('.bicicleta-imagens')
-
-function trocarImagem(event){
+function trocarImagem(event) {
   const img = event.currentTarget;
-  const media = matchMedia('(min-width:1000px)').matches
-
-  if(media){
-    galeriaContainer.prepend(img)
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
   }
-
 }
 
-function eventosGaleria(img){
-  img.addEventListener('click', trocarImagem)
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
 }
 
-galeria.forEach(eventosGaleria)
-
+galeria.forEach(eventosGaleria);
 
 // Animação
-if(window.SimpleAnime){
-  new SimpleAnime()
+if (window.SimpleAnime) {
+  new SimpleAnime();
 }
